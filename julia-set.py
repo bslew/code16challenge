2ceaede17df0351188a5b9ce1d20c0ab6f83c306
Bartosz Lew bartosz.lew@protonmail.com <blew@astro.uni.torun.pl>
2021-02-02 16:17:30 +0100

    comments

--- julia-set.py
+++ julia-set.py
@@ -1,3 +1,12 @@
+'''
+This program shows how to generata a nice Julia set 
+plot just in 16-lines of code
+
+Author: Bartosz Lew <bartosz.lew@protonmail.com>
+Created on: 02 Feb 2021
+
+'''
+
 import matplotlib.pyplot as plt
 import numpy as np # 2 lines for imports
 
@@ -16,4 +25,5 @@ fig=plt.figure(figsize=(10,10))
 plt.imshow(get_frac(*(-0.74, 0.06, -0.83, -0.03),nside=1000,a0=0+0j,blew_offset=complex(0.4284302415491922,0.14950325954226107),thres=80),cmap=plt.get_cmap('jet'), interpolation='bilinear')
 plt.axis('off')
 plt.subplots_adjust(left=0, right=1,bottom=0,top=1) # 4 lines for nice plot
+
 plt.savefig("fractal.jpg",dpi=300) # 1 line for saving image
